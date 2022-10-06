c1491ca94e66fee01c00e2fc8073eae6a5764844
Davide Ciacciolo <davide.ciacciolo@outlook.com>
2022-10-06 16:52:46 +0200

    dunst configuration added

--- install.sh
+++ install.sh
@@ -10,3 +10,6 @@ ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
 ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
+
+mkdir -p "$XDG_CONFIG_HOME/dunst"
+ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
